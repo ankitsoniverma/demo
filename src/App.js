bde17824962d78456f60901ef31448c2bf73e495
ankitsoniverma <ankitsoniverma614@gmail.com>
2026-07-04 21:48:34 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Login from  './components/Login';
+
 function App() {
   return (
 
-    <Login />
+    <div>
+      <h1>APP</h1>
+    </div>
   );
 }
 
