3b909362c52db010d9939e812e945e521fc70a05
ankitsoniverma <ankitsoniverma614@gmail.com>
2026-07-04 21:25:30 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          
-        </Routes>
-          
-         
-       
-      </BrowserRouter>
-   
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ankit
+        </a>
+      </header>
+    </div>
   );
 }
 
